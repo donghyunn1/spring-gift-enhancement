9580373ee62316ff6985f40c497023ccecdca58f
양동현 <dongdong0516@naver.com>
2025-07-14 14:14:01 +0900

    feat: 위시리스트 코드 가져오기

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE products (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    image_url VARCHAR(500)
+);
+
+CREATE TABLE members (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'USER'
+);
+
+CREATE TABLE wishes (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity BIGINT DEFAULT 1,
+    FOREIGN KEY (member_id) REFERENCES members(id),
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    UNIQUE(member_id, product_id)
+);
