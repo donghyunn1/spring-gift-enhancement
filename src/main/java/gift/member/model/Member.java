e25b55d985c015298865782ec0b64c9fb9880875
양동현 <dongdong0516@naver.com>
2025-07-15 15:41:14 +0900

    refactor: Entity 리팩터링

--- src/main/java/gift/member/model/Member.java
+++ src/main/java/gift/member/model/Member.java
@@ -1,10 +1,32 @@
 package gift.member.model;
 
+import gift.wishlist.model.Wish;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "members")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "email", unique = true)
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "role")
     private String role;
 
     public Member(Long id, String email, String password, String role) {
@@ -13,6 +35,11 @@ public class Member {
         this.password = password;
         this.role = role;
     }
+
+    protected Member() {}
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Wish> wishers = new ArrayList<>();
 
     public Member(String email, String password) {
         this(null, email, password, "USER");
--- src/main/java/gift/wishlist/model/Wish.java
+++ src/main/java/gift/wishlist/model/Wish.java
@@ -1,9 +1,28 @@
 package gift.wishlist.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "wishes",uniqueConstraints = @UniqueConstraint(columnNames = {"member_id", "product_id"}))
 public class Wish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "member_id")
     private Long memberId;
+
+    @Column(name = "product_id")
     private Long productId;
+
+    @Column(name = "quantity")
     private Long quantity;
 
     public Wish(Long id, Long memberId, Long productId, Long quantity) {
@@ -17,9 +36,7 @@ public class Wish {
         this(null, memberId, productId, quantity);
     }
 
-    public Wish(Long memberId, Long productId) {
-        this(null, memberId, productId, 1l); // 기본 수량 1
-    }
+    protected Wish() {}
 
     public Long getId() {
         return id;
